cd507de069631a898c29c57f4cea47f14144e893
John Conley <conley.john@gmail.com>
2013-10-24 14:23:51 -0700

    added faunus

--- bin/patch.sh
+++ bin/patch.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 
-usage(){
-    cat <<EOF
-$0
-
-Downloads the Rexster server distribution and the Titan HBase
-distribution. 
-
-Copies make-classpath.py into the Rexster and Titan bin dirs,
-as well as the patched rexster.sh and gremlin.sh, respectively.
-
-Replaces config/rexster.xml in the Rexster server distribution 
-with the one we've modified to use the Titan-HBase graph that
-you should create. Also copies all the jars over from the lib
-directory of Titan.
-
-EOF
-    exit 1
-}
-
 TITANVERSION=0.3.2
+FAUNUSVERSION=0.3.2
 REXSTERVERSION=2.3.0
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -33,13 +15,15 @@ fi
 if [ -d rexster-server-$REXSTERVERSION ]; then
 	rm -r rexster-server-$REXSTERVERSION
 fi
-unzip rexster-server-$REXSTERVERSION.zip
+echo unzipping rexster-server-$REXSTERVERSION.zip ...
+unzip rexster-server-$REXSTERVERSION.zip &> /dev/null
 
 if [ ! -d rexster-console-$REXSTERVERSION ]; then
 	if [ ! -f rexster-console-$REXSTERVERSION.zip ]; then
 		wget http://tinkerpop.com/downloads/rexster/rexster-console-$REXSTERVERSION.zip
 	fi
-	unzip rexster-console-$REXSTERVERSION.zip
+	echo unzipping rexster-console-$REXSTERVERSION.zip ...
+	unzip rexster-console-$REXSTERVERSION.zip &> /dev/null
 fi
 # download titan-hbase
 if [ ! -f titan-hbase-$TITANVERSION.zip ]; then
@@ -48,13 +32,28 @@ fi
 if [ -d titan-hbase-$TITANVERSION ]; then
 	rm -r titan-hbase-$TITANVERSION
 fi
-unzip titan-hbase-$TITANVERSION.zip
+echo unzipping titan-hbase-$TITANVERSION.zip ...
+unzip titan-hbase-$TITANVERSION.zip &> /dev/null
+
+# download faunus
+if [ ! -f faunus-$FAUNUSVERSION.zip ]; then
+	wget http://s3.thinkaurelius.com/downloads/faunus/faunus-$FAUNUSVERSION.zip
+fi
+if [ -d faunus-$FAUNUSVERSION ]; then
+	rm -r faunus-$FAUNUSVERSION
+fi
+echo unzipping faunus-$FAUNUSVERSION.zip ...
+unzip faunus-$FAUNUSVERSION.zip &> /dev/null
+export FAUNUS_HOME=$PWD/faunus-$FAUNUSVERSION
 
 # copy over scripts
-cp titan-mapr-patch/bin/gremlin.sh titan-hbase-$TITANVERSION/bin/
+cp titan-mapr-patch/bin/titan-gremlin.sh titan-hbase-$TITANVERSION/bin/gremlin.sh
 cp titan-mapr-patch/bin/make-classpath.py titan-hbase-$TITANVERSION/bin/
 cp titan-mapr-patch/bin/rexster.sh rexster-server-$REXSTERVERSION/bin/
 cp titan-mapr-patch/bin/make-classpath.py rexster-server-$REXSTERVERSION/bin/
+cp titan-mapr-patch/bin/faunus-gremlin.sh faunus-$FAUNUSVERSION/bin/gremlin.sh
+cp titan-mapr-patch/bin/make-classpath.py faunus-$FAUNUSVERSION/bin/
+
 
 # copy config
 if [ "$REXSTERVERSION" = "2.4.0" ]; then
@@ -70,6 +69,9 @@ cp titan-mapr-patch/bin/make-hbase-graph.groovy titan-hbase-$TITANVERSION/bin/
 sed -i.bak "s|REPLACEME|$zklist|" titan-hbase-$TITANVERSION/bin/make-hbase-graph.groovy
 sed -i.bak "s|REPLACEME|$zklist|" rexster-server-$REXSTERVERSION/${REXSTERCONFIGDIR}rexster.xml
 
+# go ahead and create graph using gremlin?
+#echo creating titan-hbase graph...
+#titan-hbase-$TITANVERSION/bin/gremlin.sh -e titan-hbase-$TITANVERSION/bin/make-hbase-graph.groovy
 
 #python titan-mapr-patch/bin/fix-jars.py titan-hbase-$TITANVERSION/lib
 
@@ -77,7 +79,7 @@ mkdir rexster-server-$REXSTERVERSION/ext/titan
 cp titan-hbase-$TITANVERSION/lib/* rexster-server-$REXSTERVERSION/ext/titan/
 # fix metrics issue
 #rm rexster-server-$REXSTERVERSION/ext/titan/metrics*
-
+# start rexster server?
 
 # avoid conflict with elasticsearch
 rm rexster-server-$REXSTERVERSION/lib/lucene-core-*.jar
@@ -88,10 +90,9 @@ $basedir/titan-hbase-$TITANVERSION
 and has been "patched," in the sense that the classpath has been
 fixed in the launch scripts.
 
-You have to now run gremlin.sh and create an HBase-backed graph. See
-$DIR/make-hbase-graph.groovy 
-for the example commands to run in the gremlin shell. 
-(TODO: run this script automatically?)
+The script will run gremlin.sh and create an HBase-backed graph. See
+titan-hbase-$TITANVERSION/bin/make-hbase-graph.groovy
+for the commands that are run in the gremlin shell. 
 
 Once the graph is created, you can configure Rexster to run with it.
 The Rexster server distribution has been downloaded to
@@ -102,7 +103,7 @@ in the sense that the classpath has been fixed in the rexster.sh script.
 The Rexster console distribution has also been downloaded to
 $basedir/rexster-console-$REXSTERVERSION
 
-To start rexster, go to the rexster server directory and run:
+To start rexster, the script goes to the rexster server directory and run:
 bin/rexster.sh -s -c ${REXSTERCONFIGDIR}rexster.xml &
 Then go to the rexster console directory and run:
 bin/rexster-console.sh
